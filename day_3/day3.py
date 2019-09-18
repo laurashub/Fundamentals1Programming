627782aa69f53a7063c22745a7a8d6bf53c87050
laurashub <shublaura@gmail.com>
2019-09-18 14:51:17 -0700

    consolidated some unnecessary functions

--- day_3/day3.py
+++ day_3/day3.py
@@ -31,13 +31,8 @@ def calc_expressions(k, clusters, exmat_df):
 	#create blank dataframe
 	result_df = pd.DataFrame(columns=range(1, k+1), index=exmat_df.index)
 	for i in range(1, k+1):
-		avg_expressions(i, clusters[i], exmat_df, result_df)
+		result_df[i] = exmat_df.loc[:,clusters[i]].mean(axis=1)
 	return result_df
-
-
-def avg_expressions(i, cell_cluster, exmat_df, result_df):
-	cluster_df = exmat_df.loc[:,cell_cluster]
-	result_df[i] = cluster_df.mean(axis=1)
 
 #main
 exmat = "E-MTAB-7365.csv" #Single-cell RNA seq expression matrix
