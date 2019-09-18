970c593124a30e8637646629d48d26b2fd2a6e21
laurashub <shublaura@gmail.com>
2019-09-18 14:30:46 -0700

    expression calculations with pandas

---
+++ day_3/day3.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+import sys
+
+#Reads in both the expression file and the clusters file we were just playing with
+def read_csv(filename, index):
+	data = pd.read_csv(filename, index_col=index) 
+	return data
+
+"""
+For the default value of k (noted by the True in the first column of the clusters file) 
+"""
+def find_k(df):
+	k_row = df[df['sel.K']]
+	return k_row.index.values[0], k_row.at[k_row.index.values[0], 'K']
+
+"""
+calculate the average expression for each gene within each cluster. 
+"""
+def cluster_cells(k_index, k, df):
+	clusters = {}
+	for i in range(1,k+1):
+		clusters[i] = []
+	for cell in df.columns:
+		if "ERR" in cell:
+			clusters[df.at[k_index, cell]].append(cell)
+	return clusters
+	
+
+def calc_expressions(k, clusters, exmat_df):
+	#create blank dataframe
+	result_df = pd.DataFrame(columns=range(1, k+1), index=exmat_df.index)
+	for i in range(1, k+1):
+		avg_expressions(i, clusters[i], exmat_df, result_df)
+	return result_df
+
+
+def avg_expressions(i, cell_cluster, exmat_df, result_df):
+	cluster_df = exmat_df.loc[:,cell_cluster]
+	#df['% Age 18-65'] = df.apply(lambda row:calculate_adults(row),axis=1)
+	result_df[i] = cluster_df.mean(axis=1)
+
+#main
+exmat = "E-MTAB-7365.csv" #Single-cell RNA seq expression matrix
+clusters = "E-MTAB-7365.clusters.csv"#cell cluster assignments for various values of k
+
+exmat_df = read_csv(exmat, 0)
+clusters_df = read_csv(clusters, False)
+#print(exmat_df.head())
+#print(clusters_df.head())
+k_index, k = find_k(clusters_df)
+clusters = cluster_cells(k_index, k, clusters_df)
+
+results_df = calc_expressions(k, clusters, exmat_df)
+print(results_df.to_string())
+
+
